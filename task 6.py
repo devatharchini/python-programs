aa8632d8b10cf71af64955b00fa85aa801918e97
devatharchini <88095187+devatharchini@users.noreply.github.com>
2021-08-04 20:23:33 +0530

    Add files via upload

---
+++ task 6.py
@@ -0,0 +1,10 @@
+a=input('please enter the string: ')
+def most_frequent(string):
+    d = dict()
+    for key in string:
+        if key in d:
+             d[key] += 1
+        else:
+            d[key] = 1
+    return d
+print(most_frequent(a))
